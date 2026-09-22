cd37668d884d40ba7788ee244dcafff04ec75f0a
Darya Sozinova <sozinova.da@gmail.com>
2026-09-22 17:30:02 +0400

    Add FilmController

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,76 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private final Map<Integer, Film> films = new HashMap<>();
+    private LocalDate startDate = LocalDate.of(1895, 12, 28);
+
+    private void validateFilm(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ValidationException("У фильма обязательно нужно указать название");
+        }
+
+        if (film.getDescription() != null && film.getDescription().length() > 200) {
+            throw new ValidationException("Описание должно иметь не более 200 символов");
+        }
+
+        if (film.getReleaseDate() != null && film.getReleaseDate().isBefore(startDate)) {
+            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
+        }
+
+        if (film.getDuration() <= 0) {
+            throw new ValidationException("Продолжительность фильма должна быть больше 0");
+        }
+    }
+
+    @GetMapping
+    public Collection<Film> findAll() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film create(@RequestBody Film film) {
+
+        validateFilm(film);
+
+        film.setId(getNextId());
+
+        films.put(film.getId(), film);
+        return film;
+    }
+
+    private int getNextId() {
+        int currentMaxId = films.keySet()
+                .stream()
+                .mapToInt(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
+
+    @PutMapping
+    public Film update(@RequestBody Film newFilm) {
+
+        validateFilm(newFilm);
+
+        if (films.containsKey(newFilm.getId())) {
+            films.put(newFilm.getId(), newFilm);
+            return newFilm;
+        } else {
+            throw new NotFoundException("Такой фильм не найден");
+        }
+    }
 }
